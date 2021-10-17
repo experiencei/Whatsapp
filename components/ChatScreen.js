fa48caaf8b6261b68a28949fb73c976cfa351cc1
experiencei <ayelojahighbee01@gmail.com>
2021-10-17 18:31:55 +0100

    getting server preps before the chatting

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,15 +1,43 @@
+import { Avatar } from "@material-ui/core";
+import { useRouter } from "next/dist/client/router";
+import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components"
+import { auth } from "../firebase";
 
-function ChatScreen() {
+function ChatScreen({ chat , messages}) {
+    const [user] = useAuthState(auth);
+    const router = useRouter();
+     
     return (
         <Container>
-            
+             <Header>
+                 <Avatar/>
+                 <HeaderInformation>
+                     <h3>Rec Email</h3>
+                     <p>lastn seen ...</p>
+                 </HeaderInformation>
+             </Header>
+             <HeaderIcons>
+
+             </HeaderIcons>
         </Container>
     )
 }
 
-export default ChatScreen
+export default ChatScreen;
 
 const Container = styled.div`
   
+`;
+
+const Header = styled.div`
+
+`;
+
+const HeaderInformation = styled.div`
+
+`;
+
+const HeaderIcons = styled.div`
+
 `;
