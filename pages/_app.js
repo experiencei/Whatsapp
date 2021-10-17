69a3ad0b50cc8dc99001514d27ed7503743d23d8
experiencei <ayelojahighbee01@gmail.com>
2021-10-17 14:24:15 +0100

    working authenticationn of login page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth , db}  from "../firebase";
 
 function MyApp({ Component, pageProps }) {
+   const [user] = useAuthState(auth);
+   
   return <Component {...pageProps} />
 }
 
