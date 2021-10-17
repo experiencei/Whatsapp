59596ba3df93df20ff1f6ff742314a3c5044e1e7
experiencei <ayelojahighbee01@gmail.com>
2021-10-17 17:38:11 +0100

    creating router component of 404

---
+++ components/ChatScreen.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+function ChatScreen() {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default ChatScreen
+
+const Container = styled.div`
+  
+`;
