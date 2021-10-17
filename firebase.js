05588ce2a132329bdf5bc95157957ef870b77df1
experiencei <ayelojahighbee01@gmail.com>
2021-10-17 14:09:31 +0100

    implementing firebase for auth and database

---
+++ firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBA5g9R75aW4jLnaD6dRM597kGVts_JIEI",
+    authDomain: "whatsappclone-89350.firebaseapp.com",
+    projectId: "whatsappclone-89350",
+    storageBucket: "whatsappclone-89350.appspot.com",
+    messagingSenderId: "196777123261",
+    appId: "1:196777123261:web:2859bb2e066065e76246d4"
+  };
+
+
+const app = !firebase.apps.length? 
+firebase.initializeApp(firebaseConfig):
+ firebase.app();
+
+ const db = app.firestore();
+ const auth = app.auth();
+ const provider = new firebase.auth.GoogleAuthProvider();
+
+ export { db , auth , provider};
