4b4ffc6bde31e4377704ada039ae201214e8c44b
experiencei <ayelojahighbee01@gmail.com>
2021-10-17 18:41:00 +0100

    getting server preps before the chatting

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,8 +1,11 @@
-import { Avatar } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
 import { useRouter } from "next/dist/client/router";
 import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components"
 import { auth } from "../firebase";
+import  MoreVertIcon from "@material-ui/icons/MoreVert";
+import  AttachFileIcon from "@material-ui/icons/AttachFile";
+
 
 function ChatScreen({ chat , messages}) {
     const [user] = useAuthState(auth);
@@ -18,8 +21,17 @@ function ChatScreen({ chat , messages}) {
                  </HeaderInformation>
              </Header>
              <HeaderIcons>
-
+                <IconButton>
+                    <AttachFileIcon/>
+                </IconButton>
+                <IconButton>
+                    <MoreVertIcon/>
+                </IconButton>
              </HeaderIcons>
+
+             <MessageContainer>
+
+             </MessageContainer>
         </Container>
     )
 }
@@ -31,10 +43,29 @@ const Container = styled.div`
 `;
 
 const Header = styled.div`
-
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top:0;
+  display: flex;
+  padding: 11px;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
 `;
 
 const HeaderInformation = styled.div`
+     margin-left: 15px;
+     flex: 1;
+
+     > h3 {
+         margin-bottom:3px;
+
+     }
+     > p {
+         font-size: 14px;
+         color: gray;
+     }
 
 `;
 
