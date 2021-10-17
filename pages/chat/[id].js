5755e30fa50d8b15b5ef313a8b1d84763b0e5293
experiencei <ayelojahighbee01@gmail.com>
2021-10-17 18:18:46 +0100

    creating router component of 404

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -2,8 +2,9 @@ import styled from "styled-components";
 import Head from "next/head";
 import Sidebar from "../../components/Sidebar";
 import ChatScreen from "../../components/ChatScreen";
+import { db } from "../../firebase";
 
-function Chat() {
+function Chat( { chat , messages}) {
     return (
         <Container>
            <Head>
@@ -20,7 +21,33 @@ function Chat() {
 export default Chat
 
 export async function getServerSideProps(context){
-   
+   const ref = db.collection("chats").doc(context.query.id);
+
+   const messagesRes = await ref.collection("messages")
+   .orderBy( "timestamp" , "asc")
+   .get();
+
+   const messages = messagesRes.docs.map( doc => ({
+        id : doc.id,
+        ...doc.data(),
+   })).map( messages => ({
+       ...messages,
+         timestamp : messages.timestamp.toDate().getTime()
+   }));
+
+//    PREP the chats
+const chatRes = await ref.get();
+const chat = {
+    id : chatRes.id,
+    ...chatRes.data()
+}
+
+  return {
+      props : {
+      messages : JSON.stringify(messages),
+      chat : chat,
+      }
+  }
 }
 
 const Container = styled.div`
