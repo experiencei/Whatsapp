b94efc56a33bb62b95e2141a51394d0a19e4b9da
experiencei <ayelojahighbee01@gmail.com>
2021-10-17 16:47:44 +0100

    working on chat side

--- components/Chat.js
+++ components/Chat.js
@@ -3,12 +3,16 @@ import { Avatar ,Button, IconButton} from "@material-ui/core";
 import getRecipientEmail from "../utilis/getRecipientEmail";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 
 function Chat({ id , users}) {
     
-    const [user] = useAuthState(auth)
-    const recipientEmail = getRecipientEmail(users, user)
+    const [user] = useAuthState(auth);
+    const [recipientSnapshot] = useCollection(db.collection("users").where('email', '==' , getRecipientEmail(users , user)));
+
+    const recipientEmail = getRecipientEmail(users, user);
+      
     return (
         <Container>
             <UserAvatar/>
