f700914d7d05927fd0fbc4642b9e858f14452ade
experiencei <ayelojahighbee01@gmail.com>
2021-10-17 17:12:38 +0100

    one to one messaging from database

--- components/Chat.js
+++ components/Chat.js
@@ -2,23 +2,27 @@ import styled  from "styled-components";
 import { Avatar ,Button, IconButton} from "@material-ui/core";
 import getRecipientEmail from "../utilis/getRecipientEmail";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../firebase";
+import { auth , db } from "../firebase";
 import { useCollection } from "react-firebase-hooks/firestore";
-
+import { useRouter } from "next/dist/client/router";
 
 function Chat({ id , users}) {
-    
+    const router = useRouter()
     const [user] = useAuthState(auth);
     const [recipientSnapshot] = useCollection(db.collection("users").where('email', '==' , getRecipientEmail(users , user)));
+
+    const enterChat = () => {
+        router.push(`/chat/${id}`)
+    }
     const recipient = recipientSnapshot?.docs?.[0]?.data();
     const recipientEmail = getRecipientEmail(users, user);
       
     return (
-        <Container>
+        <Container onClick={enterChat}>
         { recipient ? ( <UserAvatar  src={recipient?.photoURL} /> ) : (
             <UserAvatar> {recipientEmail[0]}</UserAvatar>
         )}
-            <p>{recipientEmail}</p>
+            <small>{recipientEmail}</small>
         </Container>
     )
 }
@@ -36,7 +40,7 @@ const Container = styled.div`
       background-color : #e9eaeb;
   }
 `;
-const UserAvatar = styled.div`
+const UserAvatar = styled(Avatar)`
     margin: 5px;
     margin-right:15px;
 `;
--- firebase.js
+++ firebase.js
@@ -16,7 +16,7 @@ const app = !firebase.apps.length?
 firebase.initializeApp(firebaseConfig):
  firebase.app();
 
- const db = firebase.firestore();
+ const db = app.firestore();
  const auth = app.auth();
  const provider = new firebase.auth.GoogleAuthProvider();
 
