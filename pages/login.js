1b65f1bfa2034732c38800fe05cb6bc6180b5956
experiencei <ayelojahighbee01@gmail.com>
2021-10-17 14:40:24 +0100

    working authenticationn of login page

--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,4 @@
+import { Button } from "@material-ui/core";
 import Head from "next/head";
 import styled from "styled-components";
 
@@ -12,6 +13,7 @@ function Login() {
                <Logo
                    src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
                />
+               <Button>Sign in with Google</Button>
             </LoginContainer>
         </Container>
     )
@@ -21,11 +23,18 @@ export default Login
 
 
 const Container = styled.div`
+    display: grid;
+    place-items: center;
+    height: 100vh;
 `;
 
 const LoginContainer = styled.div`
+    display: flex;
+    flex-direction: column;
 `;
 
 const Logo = styled.img`
-
+    height: 200px;
+    width: 200px;
+    margin-bottom:50px;
 `;
